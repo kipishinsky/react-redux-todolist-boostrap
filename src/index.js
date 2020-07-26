b9614e64c9ec28fded66dc4d83f828b701815e21
SS007 <rbkmoneyukk@bk.ru>
2020-07-26 14:53:33 +0400

    example jsx

--- src/index.js
+++ src/index.js
@@ -3,40 +3,50 @@ import ReactDOM from 'react-dom';
 
 
 const Todolist = () => {
-    return (
-        <ul>
-            <li>learn react</li>
-            <li>read books</li>
-        </ul>
-    )
+
+	const items = ['Drink coffee', 'Build Awesome APP']
+
+	return (
+		<ul>
+			<li> { items[0] } </li>
+			<li> { items[1] } </li>
+		</ul>
+	)
 }
 
 const AppHeader = () => {
-    return <h1>My Todo List</h1>
+	return <h1>My Todo List</h1>
 }
 
 const SearchPanel = () => {
-    return <input placeholder="searh"/>
+
+	const searchText = 'Type here to search'
+	const searchStyle = {
+		fontSize: '20px'
+	}
+
+	return <input
+		placeholder={searchText}
+		style={searchStyle}
+	/>
 }
 
 const App = () => {
-    return (
-        <div className="APP">
-            <AppHeader />
-            <SearchPanel />
-            <Todolist />
-        </div>
-    )
+
+	const isLoggedIn = true;
+	const loginBox = <span>Log in please</span>
+	const welcomeBox = <span>Welcome Back</span>
+	const value = `<script> alert('saooo')</script>`
+
+	return (
+		<div className="APP">
+			{value}
+			{ isLoggedIn ? welcomeBox : loginBox}
+			<AppHeader />
+			<SearchPanel />
+			<Todolist />
+		</div>
+	)
 }
 
-
-/*
-//равносильно const a = <h1>hello world</h1>
-const a = React.createElement('h1', null, 'hello world')
-
-virtual DOM - использование легковесных объектов, поэтому работает быстро
-
-*/
-
-
-ReactDOM.render (<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
